62400de939975bde6ec0ab989f19f121b7104325
Marek Sierociński <marek.sierocinski@bitbar.com>
2019-11-21 15:45:28 +0100

    Changing way of style changing

--- docs/main.js
+++ docs/main.js
@@ -6,7 +6,7 @@
   function submit_handler () {
     result_element.innerText = 'Bitbar';
     submit_button.innerText = 'Answered!';
-    submit_button.style = 'background-color: rgb(127, 255, 0);';
+    submit_button.style.backgroundColor = 'rgb(127, 255, 0)';
   }
 
   submit_button.addEventListener('click', submit_handler);
